651ca42c8789ea8866646d77d1ee739566e05933
Luiz <luizlhra24@gmail.com>
2022-06-08 21:39:18 -0300

    Cria models

--- projetoLP2/src/main/java/com/fundatec/lp2/Tarifa.java
+++ projetoLP2/src/main/java/com/fundatec/lp2/Tarifa.java
@@ -1,5 +1,29 @@
 package com.fundatec.lp2;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+import lombok.Data;
+
+@Entity
+@Data
 public class Tarifa {
+
+	@Column()
+	private Integer id;
+	
+	@Column(nullable=false, name="VEICULO")
+	private String Veiculo;
+	
+	@Column(nullable=false, name="ENTRADA")
+	private LocalDateTime entrada;
+	
+	@Column(nullable=false, name="SAIDA")
+	private LocalDateTime saida;
+	
+	@Column(nullable=false, name="VALOR")
+	private Double valor;
 
 }
