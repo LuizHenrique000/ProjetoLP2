9d6cc2921855cee2e182ab453f15893c7c1df12d
LuizHenrique000 <luizlhra24@gmail.com>
2022-06-16 13:36:52 -0300

    Ajusta repository

--- projetoLP2/src/main/java/com/fundatec/lp2/repository/AssinanteRepository.java
+++ projetoLP2/src/main/java/com/fundatec/lp2/repository/AssinanteRepository.java
@@ -1,5 +1,11 @@
 package com.fundatec.lp2.repository;
 
-public class AssinanteRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fundatec.lp2.models.Assinante;
+
+@Repository
+public interface AssinanteRepository extends JpaRepository<Assinante, Integer>{
 
 }
--- projetoLP2/src/main/java/com/fundatec/lp2/repository/VeiculoRepository.java
+++ projetoLP2/src/main/java/com/fundatec/lp2/repository/VeiculoRepository.java
@@ -1,5 +1,11 @@
 package com.fundatec.lp2.repository;
 
-public class VeiculoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fundatec.lp2.models.Veiculo;
+
+@Repository
+public interface VeiculoRepository extends JpaRepository<Veiculo, Integer>{
 
 }
